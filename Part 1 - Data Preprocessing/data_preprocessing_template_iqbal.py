e3d059948e1043ae6afb63e7717e2f3e1dda6c0b
Iqbal Hossain <iqbal.cse2016@gmail.com>
2018-01-11 17:26:22 +0600

    add

--- Part 1 - Data Preprocessing/data_preprocessing_template_iqbal.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template_iqbal.py
@@ -22,12 +22,23 @@ y = dataset.iloc[:, 3].values
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values= 'NaN', strategy= 'mean', axis= 0)
 imputer = imputer.fit(x[:, 1:3])
-x[:,1:3] = imputer.transform(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x = LabelEncoder()
-labelencoder_x.fit_transform(x[:, 0])
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+x = onehotencoder.fit_transform(x).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_x.fit_transform(y)
+
+
+# Splitting the dataset into Traning set and Test set
+
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=0)
 
 
 
